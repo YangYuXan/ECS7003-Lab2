d6af016ae64e057f416c5635a8f8f6cd7c65b936
YangYu <amdyuy@163.com>
2023-10-07 00:41:09 +0100

    Add function
    add debug mode

--- Assets/Game/Script/Rotater.cs
+++ Assets/Game/Script/Rotater.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class Rotater : MonoBehaviour
 {
+    public bool shouldRotate=true;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +14,10 @@ public class test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (shouldRotate == true)
+        {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
 
-    void OnTriggerEnter(Collider other)
-    {
     }
 }
